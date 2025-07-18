fd5a266e9d1aec0844bfa006e3a5a37e87154f97
Swagath Gadde <swagath.gadde@amd.com>
2025-07-18 17:31:50 +0530

    generate-machines-sdt.sh: update machine name and override
    Signed-off-by: Swagath Gadde <swagath.gadde@amd.com>
    Signed-off-by: Mark Hatle <mark.hatle@amd.com>
    (cherry picked from commit a6505f318c692b83211776bf6f72f55bb1bd0d21)
    Signed-off-by: Mark Hatle <mark.hatle@amd.com>

--- scripts/generate-machines-sdt.sh
+++ scripts/generate-machines-sdt.sh
@@ -2,7 +2,7 @@
 
 ### The following table controls the automatic generated of the machine .conf files (lines start with #M#)
 ### Machine               MULTICONFIGS                                           OVERLAY  DOMAIN   OVERRIDES       PRE   POST
-#M# emb-plus-ve2302-sdt   default       					 none     default  emb-plus-ve2302 none  none
+#M# emb-plus-ve2302-xrt   default       					 none     default  none            none  none
 #M# emb-plus-ve2302-amr   --add-config\ CONFIG_YOCTO_BBMC_CORTEXR5_0_FREERTOS=y  full     default  emb-plus-amr    none  none
 
 this=$(realpath $0)
